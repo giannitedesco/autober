a632884004260ba75f185369c6efe6de25457b08
Gianni Tedesco <gianni@scaramanga.co.uk>
2010-03-10 09:29:04 +0000

    Generate correct functions and swtich statements for decode

--- autober/c_defns.py
+++ autober/c_defns.py
@@ -8,6 +8,10 @@ class CDefn:
 							self.name.upper())
 		else:
 			self.tagname = "TAG_%s"%self.name.upper()
+	def __iter__(self):
+		return [].__iter__()
+	def __getitem__(self, idx):
+		raise IndexError
 
 class CScalar(CDefn):
 	def __init__(self, node, parent):
@@ -34,52 +38,58 @@ class CScalar(CDefn):
 class CContainer(CDefn):
 	def __scalar(self, node, prefix = '', toplevel = True):
 		ret = []
-		for x in node:
-			if toplevel:
-				name = prefix + self.prefix + x.name
-			else:
-				name = prefix + '.' + x.name
-			if Fixed in x.__class__.__bases__:
-				r = [(name, CScalar(x, parent = node))]
-			elif x.__class__ == Template and not x.sequence:
-				r = self.__scalar(x, prefix = name,
-							toplevel = False)
-			else:
-				continue
-			ret.extend(r)
+		for (n, x) in node:
+			if x.__class__ == CScalar:
+				ret.append((n,x))
+			elif x.__class__ == CStruct:
+				ret.extend(self.__scalar(x, prefix = n,
+						toplevel = False))
 		return ret
 
 	def __ptrs(self, node, prefix = '', toplevel = True):
 		ret = []
-		for x in node:
-			name = self.prefix + x.name
-			if x.__class__ != Template:
-				continue
-			if toplevel:
-				name = prefix + self.prefix + x.name
-			else:
-				name = prefix + '.' + x.name
-			if x.sequence:
-				ret.append((name, CStructPtr(x, self)))
-			else:
-				ret.extend(self.__ptrs(x, prefix = name,
+		for (n, x) in node:
+			if x.__class__ == CStructPtr:
+				ret.append((n,x))
+			elif x.__class__ == CStruct:
+				ret.extend(self.__ptrs(x, prefix = n,
 						toplevel = False))
 		return ret
 
 	def __unions(self, node):
 		ret = []
-		for x in node:
-			if x.__class__ != Union:
-				continue
-			name = self.prefix + x.name
-			ret.append((name, CUnion(self, x)))
+		for (n, x) in node:
+			if x.__class__ == CUnion:
+				ret.append((n, x))
 		return ret
-	
+
+	def __structs(self, node):
+		ret = []
+		for (n, x) in node:
+			if x.__class__ == CStruct:
+				ret.append((n, x))
+		return ret
+
 	def __toplevel(self, node):
 		ret = []
 		for x in node:
-			continue
+			n = self.prefix + x.name
+			if x.__class__ == Template:
+				if x.sequence:
+					obj = (n, CStructPtr(x, self))
+				else:
+					obj = (n, CStruct(x, self))
+			elif x.__class__ == Union:
+				obj = (n, CUnion(x, self))
+			else:
+				obj = (n, CScalar(x, self))
+			ret.append(obj)
 		return ret
+
+	def __iter__(self):
+		return self.toplevel.__iter__()
+	def __getitem__(self, idx):
+		return self.toplevel[idx]
 
 	def __str__(self):
 		return self.name
@@ -94,20 +104,68 @@ class CContainer(CDefn):
 		if not self.is_root:
 			self.count_var = "%s_%s_count"%(self.prefix, node.name)
 		self.toplevel = []
-		self.all = []
-		self.scalar = self.__scalar(node)
-		self.ptrs = self.__ptrs(node)
-		self.unions = self.__unions(node)
-		self.all.extend(self.scalar)
-		self.all.extend(self.ptrs)
-		self.all.extend(self.unions)
+		self.toplevel = self.__toplevel(node)
+		self.scalar = self.__scalar(self)
+		self.ptrs = self.__ptrs(self)
+		self.unions = self.__unions(self)
+		self.structs = self.__structs(self)
 
-class CUnion(CDefn,CContainer):
-	def __init__(self, parent, node, prefix = '.'):
+	def write_decode(self, f):
+		for (n, x) in self.ptrs:
+			x.write_decode(f)
+		for (n, x) in self.structs:
+			x.write_decode(f)
+		f.write("static int _decode_%s(struct %s *%s, \n"%(str(self),
+								str(self),
+								str(self)))
+		f.write("\t\t\t\tconst uint8_t *ptr, size_t len)\n")
+		f.write("{\n")
+		f.write("\tstatic const unsigned int num = AUTOBER_NUM_TAGS"\
+			"(%s_tags);\n"%self)
+		f.write("\tstruct autober_constraint cons[num];\n")
+		f.write("\tstruct gber_tag tag;\n")
+		f.write("\tconst uint8_t *end;\n\n")
+
+		f.write("\tif ( !autober_constraints(%s_tags, "\
+			"cons, num, ptr, len) ) {\n"%self)
+		f.write("\t\tfprintf(stderr, \"%s_tags: constraints "\
+			"not satisified\\n\");\n"%self)
+		f.write("\t\treturn 0;\n")
+		f.write("\t}\n\n")
+
+		# TODO: Check one or more union members set
+
+		f.write("\tfor(end = ptr + len; ptr < end; "\
+			"ptr += tag.ber_len) {\n")
+		f.write("\t\tptr = ber_decode_tag(&tag, ptr, end - ptr);\n")
+		f.write("\t\tif ( NULL == ptr )\n")
+		f.write("\t\t\treturn 0;\n\n")
+		f.write("\t\tswitch(tag.ber_tag) {\n")
+		for (n, x) in self.toplevel:
+			if x.__class__ == CUnion:
+				continue
+			f.write("\t\tcase %s:\n"%x.tagname)
+			f.write("\t\t\tbreak;\n");
+		for (n, x) in self.unions:
+			for (nn, xx) in x:
+				f.write("\t\tcase %s:\n"%xx.tagname)
+				f.write("\t\t\tbreak;\n");
+		f.write("\t\tdefault:\n")
+		f.write("\t\t\tfprintf(stderr, \"Unexpected tag\\n\");\n")
+		f.write("\t\t\treturn 0;\n")
+		f.write("\t\t}\n")
+		f.write("\t}\n\n")
+
+		f.write("\treturn 0;\n")
+		f.write("}\n\n")
+		return
+
+class CUnion(CContainer, CDefn):
+	def __init__(self, node, parent, prefix = '.'):
 		CContainer.__init__(self, node, parent)
 		self.type_var = "%s_%s_type"%(parent.prefix, node.name)
 		self.choices = {}
-		for (n, x) in self.all:
+		for (n, x) in self:
 			macro = "%s_TYPE_%s"%(self.name.upper(), x.name.upper())
 			self.choices[macro] = (n, x)
 	def write_free(self, f, parent, name, indent = 1):
@@ -123,8 +181,12 @@ class CUnion(CDefn,CContainer):
 			f.write("%s\tbreak;\n"%tabs)
 		f.write("%s}\n"%tabs);
 		return
+	def write_decode(self, f):
+		raise Exception("WTF")
 
-class CStructPtr(CDefn,CContainer):
+class CStruct(CContainer,CDefn):
+	pass
+class CStructPtr(CContainer,CDefn):
 	def __init__(self, node, parent):
 		CContainer.__init__(self, node, parent, prefix = '->')
 
@@ -161,62 +223,13 @@ class CStructPtr(CDefn,CContainer):
 		if self.is_root:
 			f.write("\tfree(%s);\n"%str(self))
 		f.write("}\n\n")
-	
-	def write_decode(self, f):
-		for (n, x) in self.ptrs:
-			x.write_decode(f)
-		f.write("static int _decode_%s(struct %s *%s, \n"%(str(self),
-								str(self),
-								str(self)))
-		f.write("\t\t\t\tconst uint8_t *ptr, size_t len)\n")
-		f.write("{\n")
-		f.write("\tstatic const unsigned int num = AUTOBER_NUM_TAGS"\
-			"(%s_tags);\n"%self)
-		f.write("\tstruct autober_constraint cons[num];\n")
-		f.write("\tstruct gber_tag tag;\n")
-		f.write("\tconst uint8_t *end;\n\n")
-
-		f.write("\tif ( !autober_constraints(%s_tags, "\
-			"cons, num, ptr, len) ) {\n"%self)
-		f.write("\t\tfprintf(stderr, \"%s_tags: constraints "\
-			"not satisified\\n\");\n"%self)
-		f.write("\t\treturn 0;\n")
-		f.write("\t}\n\n")
-
-		# TODO: Check one or more union members set
-
-		f.write("\tfor(end = ptr + len; ptr < end; "\
-			"ptr += tag.ber_len) {\n")
-		f.write("\t\tptr = ber_decode_tag(&tag, ptr, end - ptr);\n")
-		f.write("\t\tif ( NULL == ptr )\n")
-		f.write("\t\t\treturn 0;\n\n")
-		f.write("\t\tswitch(tag.ber_tag) {\n")
-		for (n, x) in self.toplevel:
-			f.write("\t\tcase %s:\n"%x.tagname)
-			f.write("\t\t\tbreak;\n");
-		for (n, x) in self.unions:
-			for (nn, xx) in x.all:
-				f.write("\t\tcase %s:\n"%xx.tagname)
-				f.write("\t\t\tbreak;\n");
-		f.write("\t\tdefault:\n")
-		f.write("\t\t\tfprintf(stderr, \"Unexpected tag\\n\");\n")
-		f.write("\t\t\treturn 0;\n")
-		f.write("\t\t}\n")
-		f.write("\t}\n\n")
-
-		f.write("\treturn 0;\n")
-		f.write("}\n\n")
-		return
 
 class CDefinitions:
 	def __do_print(self, node, depth = 0):
 		indent = ''.join("\t" for x in range(depth))
-		for (n, x) in node.scalar:
-			print "%s%s"%(indent, n)
-		for (n, x) in node.ptrs:
+		for (n, x) in node:
 			print "%s%s"%(indent, n)
 			self.__do_print(x, depth + 1)
-		# TODO: print unions
 			
 	def pretty_print(self):
 		print str(self.root)
