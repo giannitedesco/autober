cde833154e8a073680799a1c7954af70c82e4734
Gianni Tedesco <gianni@scaramanga.co.uk>
2010-03-16 13:08:45 +0000

    hex_dump() now unused, remove it

--- ef_dg2.c
+++ ef_dg2.c
@@ -40,39 +40,6 @@ static int mapfile(int fd, const uint8_t **begin, size_t *sz)
 	return 1;
 }
 
-static void hex_dump(const uint8_t *tmp, size_t len, size_t llen)
-{
-	size_t i, j;
-	size_t line;
-
-	for(j = 0; j < len; j += line, tmp += line) {
-		if ( j + llen > len ) {
-			line = len - j;
-		}else{
-			line = llen;
-		}
-
-		printf("%05x : ", j);
-
-		for(i = 0; i < line; i++) {
-			if ( isprint(tmp[i]) ) {
-				printf("%c", tmp[i]);
-			}else{
-				printf(".");
-			}
-		}
-
-		for(; i < llen; i++)
-			printf(" ");
-
-		for(i = 0; i < line; i++)
-			printf(" %02x", tmp[i]);
-
-		printf("\n");
-	}
-	printf("\n");
-}
-
 #define _packed __attribute__((packed))
 struct fac_hdr {
 	uint8_t magic[4];
